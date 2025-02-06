75255a5b88b70bad3be84cd217fbe494fe4aeca3
Ekaterina <katsoina95@gmail.com>
2025-02-06 15:14:50 +0300

    error background save

--- server/src/routes/backgroundRouter.js
+++ server/src/routes/backgroundRouter.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const fs = require("fs/promises");
-const sharp = require("sharp");
-const upload = require("../middlewares/multer");
-const { Background } = require("../../db/models");
+const express = require('express');
+const fs = require('fs/promises');
+const sharp = require('sharp');
+const upload = require('../middlewares/multer');
+const { Background } = require('../../db/models');
 
 const backgroundRouter = express.Router();
 
 // 📌 Сохранение фона (изображение + цвет)
-backgroundRouter.post("/save", upload.single("file"), async (req, res) => {
+backgroundRouter.post('/save', upload.single('file'), async (req, res) => {
   try {
-    const { backgroundColor, brightness, contrast, animationColor, animationType } = req.body;
+    const { backgroundColor, brightness, contrast, animationColor, animationType } =
+      req.body;
 
     let backgroundImage = null;
     if (req.file) {
@@ -20,29 +21,29 @@ backgroundRouter.post("/save", upload.single("file"), async (req, res) => {
     }
 
     const newBackground = await Background.create({
-     image: backgroundImage,
+      image: backgroundImage,
       backgroundColor,
       brightness,
       contrast,
       animationColor,
-      animationType
+      animationType,
     });
 
     res.json(newBackground);
   } catch (error) {
-    console.error("Ошибка при сохранении фона:", error);
-    res.status(500).json({ error: "Ошибка сервера" });
+    console.error('Ошибка при сохранении фона:', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
   }
 });
 
 // 📌 Получение фонов
-backgroundRouter.get("/get", async (req, res) => {
+backgroundRouter.get('/get', async (req, res) => {
   try {
     const backgrounds = await Background.findAll();
     res.json(backgrounds);
   } catch (error) {
-    console.error("Ошибка при получении данных:", error);
-    res.status(500).json({ error: "Ошибка сервера" });
+    console.error('Ошибка при получении данных:', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
   }
 });
 
